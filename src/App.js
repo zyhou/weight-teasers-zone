9dea08157dd0763e9fc33ed34785c131bcbeeaf8
Maxime <richard.maxime9@gmail.com>
2019-04-11 21:39:04 +0200

    reoder client

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import TeaserList from "./TeaserList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [teasers, setTeasers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("http://localhost:3000/teasers");
+
+      setTeasers([
+        { id: 1, name: "teaser 1" },
+        { id: 2, name: "teaser 2" },
+        { id: 3, name: "teaser 3" }
+      ]);
+    };
+
+    fetchData();
+  }, []);
+
+  const reoder = async (sourceIndex, destinationIndex) => {
+    const result = await fetch("http://localhost:3000/reoder/", {
+      method: "PUT",
+      body: JSON.stringify({ sourceIndex, destinationIndex }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+
+    //setTeasers(result);
+  };
+
+  return (
+    <div>
+      <TeaserList teasers={teasers} reorder={reoder} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/TeaserList.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import styled from "@emotion/styled";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const Table = styled.table`
+  width: 500px;
+  margin: 0 auto;
+`;
+
+const TBody = styled.tbody`
+  border: 0;
+`;
+
+const THead = styled.thead`
+  border: 0;
+  border-bottom: none;
+  background-color: red;
+`;
+
+const Row = styled.tr`
+  ${props => (props.isDragging ? `background: blue;` : "")};
+`;
+
+const Cell = styled.td`
+  box-sizing: border-box;
+  padding: 8px;
+  width: 50%;
+`;
+
+const TableRow = ({ snapshot, provided, teaser }) => (
+  <Row
+    ref={provided.innerRef}
+    isDragging={snapshot.isDragging}
+    {...provided.draggableProps}
+    {...provided.dragHandleProps}
+  >
+    <Cell>{teaser.name}</Cell>
+  </Row>
+);
+
+class TeaserList extends Component {
+  onDragEnd = result => {
+    // dropped outside the list
+    if (
+      !result.destination ||
+      result.destination.index === result.source.index
+    ) {
+      return;
+    }
+
+    // no movement
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const { reorder } = this.props;
+    reorder(result.source.index, result.destination.index);
+  };
+
+  render() {
+    const { teasers } = this.props;
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Table>
+          <THead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </THead>
+          <Droppable droppableId="table">
+            {droppableProvided => (
+              <TBody
+                ref={ref => {
+                  this.tableRef = ref;
+                  droppableProvided.innerRef(ref);
+                }}
+                {...droppableProvided.droppableProps}
+              >
+                {teasers.map((teaser, index) => (
+                  <Draggable
+                    draggableId={teaser.id}
+                    index={index}
+                    key={teaser.id}
+                  >
+                    {(provided, snapshot) => (
+                      <TableRow
+                        provided={provided}
+                        snapshot={snapshot}
+                        teaser={teaser}
+                      />
+                    )}
+                  </Draggable>
+                ))}
+                {droppableProvided.placeholder}
+              </TBody>
+            )}
+          </Droppable>
+        </Table>
+      </DragDropContext>
+    );
+  }
+}
+
+export default TeaserList;
